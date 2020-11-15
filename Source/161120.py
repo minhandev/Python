d2cfe46b9f863d5990c01ce26802d57637addb9b
minhandev <minhan.dev@gmail.com>
2020-11-15 23:35:45 +0700

    171120

---
+++ Source/161120.py
@@ -0,0 +1,27 @@
+##Bieu thuc if
+# x = 5 + 4
+# if x==5:
+#     print(x)
+# else:
+#     print("eroor")
+
+##So sanh so thuc
+# x = 1.11 - 1.10
+# y = 2.11 - 2.10
+# print('x', x , " ", "y", y )
+# diff = x-y
+# if diff < 0:
+#     diff = - diff
+# if diff < 0.0000001:
+#     print("diff < 0.0000001:")
+# else:
+#     print("!=")
+
+##Su dung if else nhu phep gan
+# x = 2 
+# y = 3
+# if x != y:
+#     c = 113
+# else:
+#     c = 115
+# print(c)
---
+++ Source/171120.py
@@ -0,0 +1,6 @@
+##while
+x = 1
+# while x < 1 or x > 10:
+#     x = int(input("Nhap gia tri: "))
+# print("x", pow(x,2))
+# print(x**2)
