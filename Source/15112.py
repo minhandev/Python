0c56bea247c9b7f35306fee439839a31445cfa43
minhandev <minhan.dev@gmail.com>
2020-11-15 20:18:50 +0700

    151120

---
+++ Source/15112.py
@@ -0,0 +1,19 @@
+# #z = 2+3j  #Kieu so thuc, 2 la phan thuc 3 la phan ao
+# z = complex(2,3)
+# print(z.real)
+# print(z.imag)
+
+#Khai bao bien o day ko can khai bao kieu du lieu
+#vd: 
+# x = 5
+# #Kiem tra kieu du lieu cua 5 la gi su dung TYPE()
+# print(type(x)) #Console: <class 'int'>
+
+# #Xoa bien su dung DEL 
+# x = 5
+# del x
+# print(type(x)) #name 'x' is not defined
+
+#Kiem tra vung du lieu import sys
+# import sys
+# print(sys.int_info)
